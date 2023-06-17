c4a0c1f71037629259996669e5f6d5f04f3f8d46
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-06-17 18:14:12 +0530

    CHORE: naming

--- Day02/countInversions.cpp
+++ Day02/countInversions.cpp
@@ -9,15 +9,15 @@ SC - O(1)
 #include <vector>
 using namespace std;
 
-long long getInversions(long long *arr, int n){
-   long long cnt = 0;
-   for(int i = 0;i<n-1;i++){
-       for(int j = i+1;j<n;j++){
-           if(arr[i] > arr[j]) cnt++;
-       }
-   }
-   return cnt;
-}
+// long long getInversions(long long *arr, int n){
+//    long long cnt = 0;
+//    for(int i = 0;i<n-1;i++){
+//        for(int j = i+1;j<n;j++){
+//            if(arr[i] > arr[j]) cnt++;
+//        }
+//    }
+//    return cnt;
+// }
 
 
 /*
