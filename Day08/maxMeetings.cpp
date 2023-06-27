ee6b56a449052d090e5f3f0a7f7cc7233b548979
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-06-27 20:19:05 +0530

    Greedy Algorithm
    add meetings and platforms problem

---
+++ Day08/maxMeetings.cpp
@@ -0,0 +1,78 @@
+// N meetings in one room
+// Problem Statement: There is one meeting room in a firm. You are given two arrays, start and end each of size N.For an index ‘i’, start[i] denotes the starting time of the ith meeting while end[i]  will denote the ending time of the ith meeting. Find the maximum number of meetings that can be accommodated if only one meeting can happen in the room at a  particular time. Print the order in which these meetings will be performed.
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct meeting
+{
+    int start;
+    int end;
+    int pos;
+};
+
+/* custom comparator -  return false - if you want to swap */
+bool static comp(struct meeting m1, struct meeting m2)
+{
+    if (m1.end != m2.end)
+    {
+        return m1.end < m2.end;
+    }
+    else
+        return m1.pos < m2.pos;
+}
+
+/*
+Time Complexity - O(nlogn)+O(n)+O(n)~O(nlogn)
+Space Complexity - O(n)  { since we used an additional data structure for storing the start time, end time, and meeting no.}
+*/
+
+void maximumMeetings(int start[], int end[], int n)
+{
+    struct meeting meet[n];
+    // O(n)
+    for (int i = 0; i < n; ++i)
+    {
+        meet[i].end = end[i];
+        meet[i].start = start[i];
+        meet[i].pos = i + 1;
+    }
+    // O(nlogn)
+    sort(meet, meet + n, comp);
+
+    vector<int> ans;
+    vector<pair<int, int>> meetings;
+    meetings.push_back({meet[0].start, meet[0].end});
+        ans.push_back(meet[0].pos);
+    int limit = meet[0].end;
+    // O(n)
+    for (int i = 1; i < n; i++)
+    {
+        if (meet[i].start > limit)
+        {
+            limit = meet[i].end;
+            ans.push_back(meet[i].pos);
+            meetings.push_back({meet[i].start, meet[i].end});
+        }
+    }
+    cout << "The order in which the meetings will be performed is " << endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << "\nPrinting the meetings intervals: " << endl;
+    for(int i = 0;i<meetings.size();i++){
+        cout << "(" << meetings[i].first << "," << meetings[i].second << ") ";
+    }
+}
+
+int main()
+{
+    int n = 6;
+    int start[] = {1, 3, 0, 5, 8, 5};
+    int end[] = {2, 4, 5, 7, 9, 9};
+    maximumMeetings(start, end, n);
+    return 0;
+}
---
+++ Day08/minPlatforms.cpp
@@ -0,0 +1,118 @@
+/*Minimum number of platforms required for a railway
+Problem Statement: We are given two arrays that represent the arrival and departure times of trains that stop at the platform. We need to find the minimum number of platforms needed at the railway station so that no train has to wait.*/
+
+/*BRUTE FORCE - we just need to think of whether a train can interrupt some other train timing of departure
+TC - O(n^2)
+SC - O(1)
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+// this works only when sorted
+int countPlatforms1(int arr[], int dep[], int n)
+{
+    sort(arr, arr+n);
+    sort(dep, dep+n);
+    int ans = 1; // final value
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int count = 1; // count of overlapping interval of only this   iteration
+        for (int j = i + 1; j <= n - 1; j++)
+        {
+            if(arr[j] >= arr[i] && arr[j] <= dep[i]){
+                count++;
+            }
+        }
+        ans = max(ans, count); // updating the value
+    }
+    return ans;
+}
+
+/*BETTER APPROACH
+- don't say in interview
+ */
+bool static comp(pair<int, int> a, pair<int, int> b)
+{
+    if (a.first != b.first)
+    {
+        return a.first < b.first;
+    }
+    else
+        return a.second < b.second;
+}
+
+int calculateMinPatforms(int at[], int dt[], int n)
+{
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    vector<pair<int, int>> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back({at[i], dt[i]});
+    }
+
+    sort(v.begin(), v.end(), comp);
+
+    int platforms = 1;
+    pq.push(v[0].second);
+
+    for (int i = 1; i < n; ++i)
+    {
+        if (v[i].first > pq.top())
+        {
+            pq.pop();
+        }
+        else
+        {
+            platforms++;
+        }
+        pq.push(v[i].second);
+    }
+    return platforms;
+}
+
+/*
+OPTIMAL SOLUTION - sorting
+- because of sorting we can keep track of train which are not departed yet so that by checking arrival time we can confirm if we need an extra platform
+TC - O(2nlogn) + O(2n)
+SC - O(1)
+*/
+
+int countPlatforms2(int at[], int dt[], int n)
+{
+    sort(at, at + n);
+    sort(dt, dt + n);
+
+    int i = 1, j = 0;
+    int plat_curr = 1;
+    int plat_needed = 1;
+    while (i < n && j < n)
+    {
+        if (at[i] <= dt[j])
+        {
+            plat_curr++;
+            i++;
+        }
+        else if (at[i] > dt[j])
+        {
+            plat_curr--;
+            j++;
+        }
+        plat_needed = max(plat_needed, plat_curr);
+    }
+    return plat_needed;
+}
+
+int main()
+{
+    int arr[] = {900, 945, 955, 1100, 1500, 1800};
+    int dep[] = {920, 1200, 1130, 1150, 1900, 2000};
+    int n = sizeof(dep) / sizeof(dep[0]);
+    cout << "Minimum number of Platforms required " << countPlatforms1(arr, dep, n) << endl;
+}
