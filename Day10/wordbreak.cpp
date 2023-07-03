77b8f0b59205cbc26071d3c1ed342edfa81fa5b4
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-07-03 16:40:00 +0530

    word break

---
+++ Day10/wordbreak.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    bool solve(int index, string s, map<string, bool> &mpp){
+        if(index >= s.size()){
+            return true;
+        }
+        for(int i = index;i<s.size();i++){
+            string sub = s.substr(index, i - index + 1);
+            if(mpp.find(sub) != mpp.end()){
+                if(solve(i + 1, s, mpp) == true){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        map<string, bool> mpp;
+        for(auto it: wordDict){
+            mpp[it] = true;
+        }
+        return solve(0, s, mpp);
+    }
+};
+
+int main(){
+    string s = "applepenapple";
+    vector<string> wordDict = {"apple", "pen"};
+    Solution obj;
+    if(obj.wordBreak(s, wordDict)){
+        cout << "possible to break";
+    } else cout << "not possible";
+
+    return 0;
+}
